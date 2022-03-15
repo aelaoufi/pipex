983e11d8dab4bdd97143679e1636d19084404f7f
Anass Elaoufi <aelaoufi@e2r8p1.1337.ma>
2022-03-15 18:45:39 +0100

    done

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: aelaoufi <aelaoufi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 13:51:10 by aelaoufi          #+#    #+#             */
-/*   Updated: 2022/03/15 18:27:46 by aelaoufi         ###   ########.fr       */
+/*   Updated: 2022/03/15 18:45:34 by aelaoufi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,6 @@
 # include <fcntl.h>
 # include <stdlib.h>
 # include <sys/wait.h>
-
-typedef struct s_var
-{
-	int	pid1;
-	int	pid2;
-	int	fd[2];
-	int	inputfile;
-	int	outputfile;
-}				t_var;
 
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
 char		**ft_split(char const *s, char c);
