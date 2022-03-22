adb4721142ae46577b930f3f951ce66643109810
Anass Elaoufi <aelaoufi@e2r9p1.1337.ma>
2022-03-22 14:17:15 +0100

    done

--- ft_pipex.c
+++ ft_pipex.c
@@ -6,17 +6,17 @@
 /*   By: aelaoufi <aelaoufi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 16:45:50 by aelaoufi          #+#    #+#             */
-/*   Updated: 2022/03/15 18:44:22 by aelaoufi         ###   ########.fr       */
+/*   Updated: 2022/03/19 15:55:30 by aelaoufi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char *find_path(char **env)
+char	*find_path(char **env)
 {
 	while (*env)
 	{
-		if (!ft_strncmp(*env ,"PATH=", 5))
+		if (!ft_strncmp(*env, "PATH=", 5))
 			return (*env + 5);
 		env++;
 	}
@@ -24,71 +24,89 @@ char *find_path(char **env)
 	return (0);
 }
 
-void exec_path(char **env, char *cmd)
+void	exec_path(char **env, char *cmd)
 {
 	char	*patt;
 	char	**paths;
 	char	**cmds;
 	int		i;
-	
+
 	i = 0;
 	patt = find_path(env);
 	paths = ft_split(patt, ':');
 	cmds = ft_split(cmd, ' ');
+	if (access(cmds[0], X_OK) == 0)
+		execve(cmds[0], cmds, env);
 	while (paths[i])
 	{
 		paths[i] = ft_strjoin(paths[i], "/");
 		paths[i] = ft_strjoin(paths[i], cmds[0]);
-		//printf("%s\n",paths[i]);
 		if (access(paths[i], X_OK) == 0)
 			execve(paths[i], cmds, env);
 		i++;
 	}
+	perror("Commande");
+}
+
+int	openpipe(char **av, t_var *p)
+{
+	p->inputfile = open(av[1], O_RDONLY);
+	if (p->inputfile < 0)
+	{
+		perror(av[1]);
+		exit(0);
+	}
+	p->outputfile = open(av[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	if (p->outputfile < 0)
+		perror(av[4]);
+	if (pipe(p->fd) == -1)
+		return (1);
+	return (0);
+}
+
+int	childprocess(char **env, char **av, t_var *p)
+{
+	p->pid1 = fork();
+	if (p->pid1 < 0)
+		return (2);
+	if (p->pid1 == 0)
+	{
+		dup2(p->fd[1], STDOUT_FILENO);
+		dup2(p->inputfile, STDIN_FILENO);
+		close(p->fd[0]);
+		close(p->fd[1]);
+		exec_path(env, av[2]);
+	}
+	p->pid2 = fork();
+	if (p->pid2 < 0)
+		return (3);
+	if (p->pid2 == 0)
+	{
+		dup2(p->fd[0], STDIN_FILENO);
+		dup2(p->outputfile, STDOUT_FILENO);
+		close(p->fd[0]);
+		close(p->fd[1]);
+		exec_path(env, av[3]);
+	}
+	return (0);
 }
 
 int	main(int ac, char **av, char **env)
 {
-	int pid1;
-	int fd[2];
-	int inputfile;
-	int outputfile;
-	
+	t_var	*p;
+
 	if (ac != 5)
 	{
 		perror("Wrong number or arguments");
 		return (0);
 	}
-	
-	inputfile = open(av[1], O_RDONLY);
-	outputfile = open(av[4], O_CREAT | O_RDWR | O_TRUNC, 0777);
 	(void)ac;
-	if  (pipe(fd) == -1)
-		return 1;
-	pid1 = fork();
-	if (pid1 < 0)
-		return 2;
-	if (pid1 == 0)
-	{
-		//child process 1 
-		dup2(fd[1], STDOUT_FILENO);
-		dup2(inputfile, STDIN_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		exec_path(env, av[2]);
-	}
-	int pid2 = fork();
-	if (pid2 < 0)
-		return 3;
-	if (pid2 == 0)
-	{
-		dup2(fd[0], STDIN_FILENO);
-		dup2(outputfile, STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		exec_path(env, av[3]);
-	}
-	waitpid(pid1, NULL, 0);
-	close(fd[0]);
-	close(fd[1]);
-	waitpid(pid2, NULL, 0);
+	p = malloc(sizeof(t_var));
+	openpipe(av, p);
+	childprocess(env, av, p);
+	close(p->fd[0]);
+	close(p->fd[1]);
+	while (wait(NULL) != -1)
+		;
+	free(p);
 }
